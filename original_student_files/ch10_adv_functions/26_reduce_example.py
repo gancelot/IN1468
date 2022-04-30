6a064004bd26d41d8dba1dc63b3c3ba8cca46263
R G <gancelot@yahoo.com>
2022-04-30 10:05:05 -0600

    Add files via upload

---
+++ original_student_files/ch10_adv_functions/26_reduce_example.py
@@ -0,0 +1,12 @@
+"""
+    26_reduce_example.py
+    Using reduce()
+"""
+import functools
+
+
+def sentence_maker(word1, word2):
+    return ' '.join([word1, word2])
+
+results = functools.reduce(sentence_maker, ['Four', 'score', 'and', 'seven', 'years', 'ago'])
+print(results)
