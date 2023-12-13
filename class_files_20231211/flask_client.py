ac8411645d5fae2792683d5fc32f00b3d73c7f7b
R G <gancelot@yahoo.com>
2023-12-13 13:30:43 -0700

    Add files via upload

---
+++ class_files_20231211/flask_client.py
@@ -0,0 +1,16 @@
+import click
+import requests
+
+
+school_name = click.prompt('Enter a school name', default='Loyola')
+
+url = f'http://localhost:8051/school/{school_name}'
+
+r = requests.get(url)
+print('Our client HTTP headers:\n', r.request.headers)
+print('Flask server HTTP headers:\n', r.headers)
+print('The Raw JSON response values:\n', r.text)
+results = r.json()
+print(f'Results for: {results.get("school_name")}')
+for school in results.get('schools', []):
+    print(school)
