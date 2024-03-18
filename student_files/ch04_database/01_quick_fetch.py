80bcfa9a8ea7f3d623963869c5a9847f9403897d
R G <gancelot@yahoo.com>
2024-03-18 12:43:47 -0600

    Add files via upload

---
+++ student_files/ch04_database/01_quick_fetch.py
@@ -0,0 +1,12 @@
+"""
+
+    01_quick_fetch.py
+
+"""
+import sqlite3
+
+conn = sqlite3.connect('course_data.db')
+cursor = conn.cursor()
+cursor.execute('select fullname, city, state from schools where state="CO"')
+results = cursor.fetchmany(size=3)
+print(results)
