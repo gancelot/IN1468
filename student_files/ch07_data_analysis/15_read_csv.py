4c3d61999fe9dbc38a078b479923c57e3eabdc63
R G <gancelot@yahoo.com>
2025-10-02 20:51:29 -0600

    Delete student_files/ch07_data_analysis directory

--- student_files/ch07_data_analysis/15_read_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-pd.set_option('display.max_columns', 10)
-pd.set_option('display.width', 1000)
-
-contacts = pd.read_csv('contacts.dat', header=None, dtype='str')
-contacts.columns = ['name', 'address', 'state', 'zip', 'area_code', 'phone', 'email', 'company', 'position']
-
-print(contacts)
